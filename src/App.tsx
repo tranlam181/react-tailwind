5cc2ef2a7a3dc9eccdd8703da6ae649f8bbfae4a
Lam Le <lam.le@marahub.com>
2021-01-05 08:19:26 +0700

    x

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         </a>
       </header>
       <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700 lg:w-40">
-        Click me
+        Click me xxx
       </button>
     </div>
   );
